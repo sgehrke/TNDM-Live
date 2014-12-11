a7d1880148edabe9b8e77eb6a1542d8b16455eef
sgehrke <shaungehrke@ShaunGerkesiMac.eau.wi.charter.com>
2014-12-10 23:19:54 -0600

    Total Redesgn
    Mad the site more full width, included a sign in modal and much more

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
+
+
 $(document).ready(function(){
-
-
-var divs = $('a.button1');
-$(window).scroll(function(){
-   if($(window).scrollTop()<10){
-         divs.stop(true,true).fadeIn();
-   } else {
-         divs.stop(true,true).fadeOut();
-   }
+	$("#login-content").hide();
+	
+	$('#login-trigger').click(function(event){
+		//also can us .is() -this will test if jquery is currently changing the element
+		//example with a conditional 
+		/*
+		if ( !$('#login-content').is(':animated')){
+			$('#login-content').slideToggle();
+		};
+			This will make for a smoother effect because it will disable all  user interaction until the animation completes
+			Unfortunately this way will not work in the instance due the hide animation that is called before this function
+*/
+			$('#login-content').stop(true, true).slideToggle();
+	});
 });
 
-});
+
+
+
